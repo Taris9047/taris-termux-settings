906fdc6306a3cf10c5394e0f7985f8b0ae4b5004
Taris <my@email.com>
2026-04-24 23:11:33 +0900

    Additional packages for proot ubuntu

--- ubuntu-24.04/setup-proot-ubuntu-24.04.sh
+++ ubuntu-24.04/setup-proot-ubuntu-24.04.sh
@@ -4,9 +4,36 @@
 # Linux, in this case: Ubuntu 24.04
 #
 
+#
+# Package list
+#
+inst_pkg_list=(
+	"neofetch"
+	"build-essential"
+	"cmake"
+	"curl"
+	"wget"
+	"m4"
+	"flex"
+	"bison"
+)
+
+#
+# Termux (one that wrapping this linux) paths
+#
+TERM_PREFIX='/data/data/com.termux/files/usr/'
+TERM_HOME='/data/data/com.termux/files/home/'
+SETTINGS_DIR="${TERM_HOME}/.settings"
+
+#
 # Curtesy update
 #
 printf '>>>> In Proot: Running Curtesy Updates\n'
 sudo apt update && sudo updgrade -y
 
+#
+# Installing the packages from the package list
+#
+printf '>>>> In Proot: Installing some packages..\n'
+sudo apt install -y "${inst_pkg_list[@]}"
 
