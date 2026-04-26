0be57a33e56d42306fa60ab0574c4cf7ee5dbd73
Taris <talezshin@gmail.com>
2026-04-26 21:33:38 +0900

    More parameters to make it silent

--- utils/install_miniconda.sh
+++ utils/install_miniconda.sh
@@ -10,34 +10,38 @@ URL="https://repo.anaconda.com/miniconda/${INSTALLER}"
 
 echo ">>> Starting Miniconda installation for ${PROOT_USER} inside ${PROOT_USER}"
 
+echo '>>> Making sure proot distro has wget'
+proot-distro login "${DISTRO}" --user "${PROOT_USER}" -- bash -c "apt update && apt install -y wget"
+
 proot-distro login "${DISTRO}" --user "${PROOT_USER}" -- bash -c "
   # Download the installer 
   echo '>>>> Downloading Miniconda...'
-  wget -q \"${URL}\" -O \"~/${INSTALLER}\"
+  wget -q \"${URL}\" -O \"\${HOME}/${INSTALLER}\"
 
   # Run installer silently
   echo '>>>> Installing Miniconda'
-  bash \"~/${INSTALLER}\" -b -p ~/.miniconda3
+  bash \"\${HOME}/${INSTALLER}\" -b -p \${HOME}/.miniconda3
 
   # Cleaning up the installer
-  rm -rfv \"~/${INSTALLER}\"
+  rm -rfv \"\${HOME}/${INSTALLER}\"
 
   # Initialize conda for bash
   echo '>>>> Initializing conda...'
-  ~/.miniconda3/bin/conda init bash
+  \${HOME}/.miniconda3/bin/conda init bash
 
 "
 
 # Now set up miniconda 
 echo ">>> Setting up Miniconda3"
 proot-distro login "${DISTRO}" --user "${PROOT_USER}" -- bash -ci "
-  conda install -n base conda-libmamba-solver
+  conda install -n base conda-libmamba-solver -y
   conda config --set solver libmamba
   conda update --all -y
+  conda config --set plugins.auto_accept_tos yes
 "
 
 echo ">>> Setting up Jupyter-Lab for Miniconda3"
 proot-distro login "${DISTRO}" --user "${PROOT_USER}" -- bash -ci "
-  conda create -n jlab numpy scipy matplotlib jupyterlab pandas h5py
+  conda create -n jlab numpy scipy matplotlib jupyterlab pandas h5py -y
 "
 
